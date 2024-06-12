f0205be20ebe51ac8dc4555810d0a0088c9394c3
xiao hu <xiao3425@gmail.com>
2024-06-12 11:17:28 -0400

    Completed Product class implementation

---
+++ ProductProject/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Product
+    {
+        public int ProductID {  get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int Stock {  get; set; }
+
+        // empty parameter constructor 
+        public Product() { }
+
+        // full parameter constructor 
+        public Product(int productID, string productName, double price, int stock)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+
+
+        // Increase the stock
+        public void Increase(int stockIn)
+        {
+            Stock += stockIn;
+        }
+
+        // decrease the stock
+        public void Decrease(int stockOut) {
+            if (stockOut <= Stock)
+            {
+                Stock -= stockOut;
+            }
+            else
+            {
+                throw new ArgumentException("Not enough stock.");
+            }
+        }
+    }
+}
