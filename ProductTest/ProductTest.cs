5bf91d77e8e1f77baa889ac19259e02e107d5bc4
xiao hu <xiao3425@gmail.com>
2024-06-13 11:43:47 -0400

    Complete unit tests for Product class

--- ProductTest/ProductTest.cs
+++ ProductTest/ProductTest.cs
@@ -1,4 +1,4 @@
-﻿using Assignment2;
+﻿using ECommerceApp;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProductProject
+namespace ECommerceApp
 {
     public class ProductTests
     {
--- ProductTest/Program.cs
+++ ProductTest/Program.cs
@@ -1,4 +1,4 @@
-namespace ProductTest
+namespace ECommerceApp
 {
     public class Tests
     {
